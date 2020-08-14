9ae87fa904a4bd4edfbf58b972535577d6e0e73a
Austin Shoecraft <ashoecraft@jibe.com>
2020-08-14 10:07:23 -0400

    feat: update actions to use ngrx action creator functions

--- src/actions.ts
+++ src/actions.ts
@@ -1,10 +1,39 @@
-import { Action } from '@ngrx/store';
-import { KeyValue, NgrxFormControlId, ValidationErrors } from './state';
+import {Action, createAction, props} from '@ngrx/store';
+import { NgrxFormControlId, ValidationErrors } from './state';
 
 // NOTE: the explicit type declaration for the `TYPE` properties is required
 // for the output declarations to properly use the literal string type instead
 // of just `string`
 
+export enum ALL_NGRX_FORMS_ACTION_TYPES {
+  SetValueActionType = 'ngrx/forms/SET_VALUE',
+  SetErrorsActionType = 'ngrx/forms/SET_ERRORS',
+  SetAsyncErrorsActionType = 'ngrx/forms/SET_ASYNC_ERROR',
+  ClearAsyncErrorActionType = 'ngrx/forms/CLEAR_ASYNC_ERROR',
+  StartAsyncValidationActionType = 'ngrx/forms/START_ASYNC_VALIDATION',
+  MarkAsDirtyActionType = 'ngrx/forms/MARK_AS_DIRTY',
+  MarkAsPristineActionType = 'ngrx/forms/MARK_AS_PRISTINE',
+  DisableActionType = 'ngrx/forms/DISABLE',
+  MarkAsTouchedActionType = 'ngrx/forms/MARK_AS_TOUCHED',
+  MarkAsUnTouchedActionType = 'ngrx/forms/MARK_AS_UNTOUCHED',
+  FocusActionType = 'ngrx/forms/FOCUS',
+  UnfocusActionType = 'ngrx/forms/UNFOCUS',
+  MarkAsSubmittedType = 'ngrx/forms/MARK_AS_SUBMITTED',
+  MarkAsUnsubmittedType = 'ngrx/forms/MARK_AS_UNSUBMITTED',
+  AddGroupControlActionType = 'ngrx/forms/ADD_GROUP_CONTROL',
+  RemoveArrayControlActionType = 'ngrx/forms/REMOVE_ARRAY_CONTROL',
+  SwapArrayControlActionType = 'ngrx/forms/SWAP_ARRAY_CONTROL',
+  MoveArrayControlActionType = 'ngrx/forms/MOVE_ARRAY_CONTROL',
+  RemoveGroupControlActionType = 'ngrx/forms/REMOVE_CONTROL',
+  SetUserDefinedPropertyActionType = 'ngrx/forms/SET_USER_DEFINED_PROPERTY',
+  ResetActionType = 'ngrx/forms/RESET',
+}
+
+export const SetValueAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.SetValueActionType,
+    props<{controlId: NgrxFormControlId, value: any}>()
+)
+/**
 export class SetValueAction<TValue> implements Action {
   static readonly TYPE: 'ngrx/forms/SET_VALUE' = 'ngrx/forms/SET_VALUE';
   readonly type = SetValueAction.TYPE;
@@ -14,7 +43,9 @@ export class SetValueAction<TValue> implements Action {
     public readonly value: TValue,
   ) { }
 }
+*/
 
+/*
 export class SetErrorsAction implements Action {
   static readonly TYPE: 'ngrx/forms/SET_ERRORS' = 'ngrx/forms/SET_ERRORS';
   readonly type = SetErrorsAction.TYPE;
@@ -24,7 +55,13 @@ export class SetErrorsAction implements Action {
     public readonly errors: ValidationErrors,
   ) { }
 }
+*/
+export const SetErrorsActions = createAction(
+  ALL_NGRX_FORMS_ACTION_TYPES.SetErrorsActionType,
+  props<{controlId: NgrxFormControlId, errors: ValidationErrors}>()
+)
 
+/*
 export class SetAsyncErrorAction implements Action {
   static readonly TYPE: 'ngrx/forms/SET_ASYNC_ERROR' = 'ngrx/forms/SET_ASYNC_ERROR';
   readonly type = SetAsyncErrorAction.TYPE;
@@ -35,7 +72,13 @@ export class SetAsyncErrorAction implements Action {
     public readonly value: any,
   ) { }
 }
+ */
 
+export const SetAsyncErrorsAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.SetAsyncErrorsActionType,
+    props<{controlId: NgrxFormControlId, name: string, value: any}>()
+)
+/*
 export class ClearAsyncErrorAction implements Action {
   static readonly TYPE: 'ngrx/forms/CLEAR_ASYNC_ERROR' = 'ngrx/forms/CLEAR_ASYNC_ERROR';
   readonly type = ClearAsyncErrorAction.TYPE;
@@ -45,7 +88,13 @@ export class ClearAsyncErrorAction implements Action {
     public readonly name: string,
   ) { }
 }
+ */
 
+export const ClearAsyncErrorAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.ClearAsyncErrorActionType,
+    props<{controlId: NgrxFormControlId, name: string, value: any}>()
+)
+/*
 export class StartAsyncValidationAction implements Action {
   static readonly TYPE: 'ngrx/forms/START_ASYNC_VALIDATION' = 'ngrx/forms/START_ASYNC_VALIDATION';
   readonly type = StartAsyncValidationAction.TYPE;
@@ -55,7 +104,13 @@ export class StartAsyncValidationAction implements Action {
     public readonly name: string,
   ) { }
 }
+*/
 
+export const StartAsyncValidationAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.StartAsyncValidationActionType,
+    props<{controlId: NgrxFormControlId, name: string}>()
+)
+/*
 export class MarkAsDirtyAction implements Action {
   static readonly TYPE: 'ngrx/forms/MARK_AS_DIRTY' = 'ngrx/forms/MARK_AS_DIRTY';
   readonly type = MarkAsDirtyAction.TYPE;
@@ -64,7 +119,14 @@ export class MarkAsDirtyAction implements Action {
     public readonly controlId: NgrxFormControlId,
   ) { }
 }
+*/
 
+export const MarkAsDirtyAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.MarkAsDirtyActionType,
+    props<{controlId: NgrxFormControlId}>()
+)
+
+/*
 export class MarkAsPristineAction implements Action {
   static readonly TYPE: 'ngrx/forms/MARK_AS_PRISTINE' = 'ngrx/forms/MARK_AS_PRISTINE';
   readonly type = MarkAsPristineAction.TYPE;
@@ -73,7 +135,12 @@ export class MarkAsPristineAction implements Action {
     public readonly controlId: NgrxFormControlId,
   ) { }
 }
-
+*/
+export const MarkAsPristineAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.MarkAsPristineActionType,
+    props<{controlId: NgrxFormControlId}>()
+)
+/*
 export class EnableAction implements Action {
   static readonly TYPE: 'ngrx/forms/ENABLE' = 'ngrx/forms/ENABLE';
   readonly type = EnableAction.TYPE;
@@ -82,7 +149,11 @@ export class EnableAction implements Action {
     public readonly controlId: NgrxFormControlId,
   ) { }
 }
-
+*/
+export const EnableAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.MarkAsDirtyActionType,
+)
+/*
 export class DisableAction implements Action {
   static readonly TYPE: 'ngrx/forms/DISABLE' = 'ngrx/forms/DISABLE';
   readonly type = DisableAction.TYPE;
@@ -91,7 +162,12 @@ export class DisableAction implements Action {
     public readonly controlId: NgrxFormControlId,
   ) { }
 }
-
+*/
+export const DisableAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.DisableActionType,
+    props<{controlId: NgrxFormControlId}>()
+)
+/*
 export class MarkAsTouchedAction implements Action {
   static readonly TYPE: 'ngrx/forms/MARK_AS_TOUCHED' = 'ngrx/forms/MARK_AS_TOUCHED';
   readonly type = MarkAsTouchedAction.TYPE;
@@ -100,7 +176,17 @@ export class MarkAsTouchedAction implements Action {
     public readonly controlId: NgrxFormControlId,
   ) { }
 }
+*/
+export const MarkAsTouchedAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.MarkAsTouchedActionType,
+    props<{controlId: NgrxFormControlId}>()
+)
 
+export const MarkAsUntouchedAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.MarkAsUnTouchedActionType,
+    props<{controlId: NgrxFormControlId}>()
+)
+/*
 export class MarkAsUntouchedAction implements Action {
   static readonly TYPE: 'ngrx/forms/MARK_AS_UNTOUCHED' = 'ngrx/forms/MARK_AS_UNTOUCHED';
   readonly type = MarkAsUntouchedAction.TYPE;
@@ -109,7 +195,8 @@ export class MarkAsUntouchedAction implements Action {
     public readonly controlId: NgrxFormControlId,
   ) { }
 }
-
+*/
+/*
 export class FocusAction implements Action {
   static readonly TYPE: 'ngrx/forms/FOCUS' = 'ngrx/forms/FOCUS';
   readonly type = FocusAction.TYPE;
@@ -118,7 +205,12 @@ export class FocusAction implements Action {
     public readonly controlId: NgrxFormControlId,
   ) { }
 }
-
+*/
+export const FocusAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.FocusActionType,
+    props<{controlId: NgrxFormControlId}>()
+)
+/*
 export class UnfocusAction implements Action {
   static readonly TYPE: 'ngrx/forms/UNFOCUS' = 'ngrx/forms/UNFOCUS';
   readonly type = UnfocusAction.TYPE;
@@ -127,7 +219,12 @@ export class UnfocusAction implements Action {
     public readonly controlId: NgrxFormControlId,
   ) { }
 }
-
+*/
+export const UnfocusAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.UnfocusActionType,
+    props<{controlId: NgrxFormControlId}>()
+)
+/*
 export class MarkAsSubmittedAction implements Action {
   static readonly TYPE: 'ngrx/forms/MARK_AS_SUBMITTED' = 'ngrx/forms/MARK_AS_SUBMITTED';
   readonly type = MarkAsSubmittedAction.TYPE;
@@ -136,7 +233,12 @@ export class MarkAsSubmittedAction implements Action {
     public readonly controlId: NgrxFormControlId,
   ) { }
 }
-
+*/
+export const MarkAsSubmittedAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.MarkAsSubmittedType,
+    props<{controlId: NgrxFormControlId}>()
+)
+/*
 export class MarkAsUnsubmittedAction implements Action {
   static readonly TYPE: 'ngrx/forms/MARK_AS_UNSUBMITTED' = 'ngrx/forms/MARK_AS_UNSUBMITTED';
   readonly type = MarkAsUnsubmittedAction.TYPE;
@@ -145,7 +247,13 @@ export class MarkAsUnsubmittedAction implements Action {
     public readonly controlId: NgrxFormControlId,
   ) { }
 }
+*/
+export const MarkAsUnsubmittedAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.MarkAsUnsubmittedType,
+    props<{controlId: NgrxFormControlId}>()
+)
 
+/*
 export class AddArrayControlAction<TValue> implements Action {
   static readonly TYPE: 'ngrx/forms/ADD_ARRAY_CONTROL' = 'ngrx/forms/ADD_ARRAY_CONTROL';
   readonly type = AddArrayControlAction.TYPE;
@@ -156,7 +264,13 @@ export class AddArrayControlAction<TValue> implements Action {
     public readonly index?: number,
   ) { }
 }
+*/
 
+export const AddGroupControlAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.AddGroupControlActionType,
+    props<{controlId: NgrxFormControlId, name: string, value: any}>()
+)
+/*
 export class AddGroupControlAction<TValue extends KeyValue, TControlKey extends keyof TValue = keyof TValue> implements Action {
   static readonly TYPE: 'ngrx/forms/ADD_GROUP_CONTROL' = 'ngrx/forms/ADD_GROUP_CONTROL';
   readonly type = AddGroupControlAction.TYPE;
@@ -167,7 +281,8 @@ export class AddGroupControlAction<TValue extends KeyValue, TControlKey extends 
     public readonly value: TValue[TControlKey],
   ) { }
 }
-
+*/
+/*
 export class RemoveArrayControlAction implements Action {
   static readonly TYPE: 'ngrx/forms/REMOVE_ARRAY_CONTROL' = 'ngrx/forms/REMOVE_ARRAY_CONTROL';
   readonly type = RemoveArrayControlAction.TYPE;
@@ -177,7 +292,12 @@ export class RemoveArrayControlAction implements Action {
     public readonly index: number,
   ) { }
 }
-
+*/
+export const RemoveArrayControlAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.RemoveArrayControlActionType,
+    props<{controlId: NgrxFormControlId, index: number}>()
+)
+/*
 export class SwapArrayControlAction implements Action {
   static readonly TYPE: 'ngrx/forms/SWAP_ARRAY_CONTROL' = 'ngrx/forms/SWAP_ARRAY_CONTROL';
   readonly type = SwapArrayControlAction.TYPE;
@@ -188,7 +308,16 @@ export class SwapArrayControlAction implements Action {
     public readonly toIndex: number
   ) { }
 }
-
+*/
+export const SwapArrayControlAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.SwapArrayControlActionType,
+    props<{
+      controlId: NgrxFormControlId,
+      fromIndex: number,
+      toIndex: number
+    }>()
+)
+/*
 export class MoveArrayControlAction implements Action {
   static readonly TYPE: 'ngrx/forms/MOVE_ARRAY_CONTROL' = 'ngrx/forms/MOVE_ARRAY_CONTROL';
   readonly type = MoveArrayControlAction.TYPE;
@@ -199,7 +328,17 @@ export class MoveArrayControlAction implements Action {
     public readonly toIndex: number
   ) { }
 }
+*/
 
+export const MoveArrayControlAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.MoveArrayControlActionType,
+    props<{
+      controlId: NgrxFormControlId,
+      fromIndex: number,
+      toIndex: number
+    }>()
+)
+/*
 export class RemoveGroupControlAction<TValue> implements Action {
   static readonly TYPE: 'ngrx/forms/REMOVE_CONTROL' = 'ngrx/forms/REMOVE_CONTROL';
   readonly type = RemoveGroupControlAction.TYPE;
@@ -209,7 +348,16 @@ export class RemoveGroupControlAction<TValue> implements Action {
     public readonly name: keyof TValue,
   ) { }
 }
+*/
 
+export const RemoveGroupControlAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.RemoveGroupControlActionType,
+    props<{
+        controlId: NgrxFormControlId,
+        name: string
+    }>()
+)
+/*
 export class SetUserDefinedPropertyAction implements Action {
   static readonly TYPE: 'ngrx/forms/SET_USER_DEFINED_PROPERTY' = 'ngrx/forms/SET_USER_DEFINED_PROPERTY';
   readonly type = SetUserDefinedPropertyAction.TYPE;
@@ -220,7 +368,18 @@ export class SetUserDefinedPropertyAction implements Action {
     public readonly value: any,
   ) { }
 }
+*/
 
+export const SetUserDefinedPropertyAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.SetUserDefinedPropertyActionType,
+    props<{
+        controlId: NgrxFormControlId,
+        name: string,
+        value: any
+    }>()
+)
+
+/*
 export class ResetAction implements Action {
   static readonly TYPE: 'ngrx/forms/RESET' = 'ngrx/forms/RESET';
   readonly type = ResetAction.TYPE;
@@ -229,59 +388,64 @@ export class ResetAction implements Action {
     public readonly controlId: NgrxFormControlId,
   ) { }
 }
+*/
 
-export type Actions<TValue> =
-  | SetValueAction<TValue>
-  | SetErrorsAction
-  | SetAsyncErrorAction
-  | ClearAsyncErrorAction
-  | StartAsyncValidationAction
-  | MarkAsDirtyAction
-  | MarkAsPristineAction
-  | EnableAction
-  | DisableAction
-  | MarkAsTouchedAction
-  | MarkAsUntouchedAction
-  | FocusAction
-  | UnfocusAction
-  | MarkAsSubmittedAction
-  | MarkAsUnsubmittedAction
-  | AddGroupControlAction<TValue>
-  | RemoveGroupControlAction<TValue>
-  | AddArrayControlAction<any>
-  | RemoveArrayControlAction
-  | SetUserDefinedPropertyAction
-  | ResetAction
-  | SwapArrayControlAction
-  | MoveArrayControlAction
-  ;
+export const ResetAction = createAction(
+    ALL_NGRX_FORMS_ACTION_TYPES.ResetActionType,
+    props<{controlId: NgrxFormControlId}>()
+)
+// export type Actions<TValue> =
+//   | SetValueAction<TValue>
+//   | SetErrorsAction
+//   | SetAsyncErrorAction
+//   | ClearAsyncErrorAction
+//   | StartAsyncValidationAction
+//   | MarkAsDirtyAction
+//   | MarkAsPristineAction
+//   | EnableAction
+//   | DisableAction
+//   | MarkAsTouchedAction
+//   | MarkAsUntouchedAction
+//   | FocusAction
+//   | UnfocusAction
+//   | MarkAsSubmittedAction
+//   | MarkAsUnsubmittedAction
+//   | AddGroupControlAction<TValue>
+//   | RemoveGroupControlAction<TValue>
+//   | AddArrayControlAction<any>
+//   | RemoveArrayControlAction
+//   | SetUserDefinedPropertyAction
+//   | ResetAction
+//   | SwapArrayControlAction
+//   | MoveArrayControlAction
+//   ;
 
 export function isNgrxFormsAction(action: Action) {
   return !!action.type && action.type.startsWith('ngrx/forms/');
 }
 
-export const ALL_NGRX_FORMS_ACTION_TYPES: Actions<any>['type'][] = [
-  SetValueAction.TYPE,
-  SetErrorsAction.TYPE,
-  SetAsyncErrorAction.TYPE,
-  ClearAsyncErrorAction.TYPE,
-  StartAsyncValidationAction.TYPE,
-  MarkAsDirtyAction.TYPE,
-  MarkAsPristineAction.TYPE,
-  EnableAction.TYPE,
-  DisableAction.TYPE,
-  MarkAsTouchedAction.TYPE,
-  MarkAsUntouchedAction.TYPE,
-  FocusAction.TYPE,
-  UnfocusAction.TYPE,
-  MarkAsSubmittedAction.TYPE,
-  MarkAsUnsubmittedAction.TYPE,
-  AddGroupControlAction.TYPE,
-  RemoveGroupControlAction.TYPE,
-  AddArrayControlAction.TYPE,
-  RemoveArrayControlAction.TYPE,
-  SetUserDefinedPropertyAction.TYPE,
-  ResetAction.TYPE,
-  SwapArrayControlAction.TYPE,
-  MoveArrayControlAction.TYPE,
-];
+// export const ALL_NGRX_FORMS_ACTION_TYPES: Actions<any>['type'][] = [
+//   SetValueAction.TYPE,
+//   SetErrorsAction.TYPE,
+//   SetAsyncErrorAction.TYPE,
+//   ClearAsyncErrorAction.TYPE,
+//   StartAsyncValidationAction.TYPE,
+//   MarkAsDirtyAction.TYPE,
+//   MarkAsPristineAction.TYPE,
+//   EnableAction.TYPE,
+//   DisableAction.TYPE,
+//   MarkAsTouchedAction.TYPE,
+//   MarkAsUntouchedAction.TYPE,
+//   FocusAction.TYPE,
+//   UnfocusAction.TYPE,
+//   MarkAsSubmittedAction.TYPE,
+//   MarkAsUnsubmittedAction.TYPE,
+//   AddGroupControlAction.TYPE,
+//   RemoveGroupControlAction.TYPE,
+//   AddArrayControlAction.TYPE,
+//   RemoveArrayControlAction.TYPE,
+//   SetUserDefinedPropertyAction.TYPE,
+//   ResetAction.TYPE,
+//   SwapArrayControlAction.TYPE,
+//   MoveArrayControlAction.TYPE,
+// ];
